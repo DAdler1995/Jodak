7ddf4f437239d3cf62f76851b1ba5b39d5cb90b1
CodeRed <zeketiki@gmail.com>
2016-08-06 11:29:19 -0700

    Initial Push - Using MVC Template in Visual Studio

---
+++ Jodak/Jodak/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jodak.Startup))]
+namespace Jodak
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
